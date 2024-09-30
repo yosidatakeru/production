39340f913751a3019387710263f51ba5f9748f6a
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2024-09-30 12:06:10 +0900

    プレイヤーの移動完了

--- Assets/Scenes/PlayerScript.cs
+++ Assets/Scenes/PlayerScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    //プレイヤーの速度
+    float speed = 6.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,30 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //プレイヤーの移動処理
+        //上に移動
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position +=  speed * transform.up * Time.deltaTime;
+        }
+        //下に移動
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= speed * transform.up * Time.deltaTime;
+        }
+        //右に移動
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += speed * transform.right * Time.deltaTime;
+        }
+        //左に移動
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= speed * transform.right * Time.deltaTime;
+        }
         
+        //弾の発射
+
+
     }
 }
