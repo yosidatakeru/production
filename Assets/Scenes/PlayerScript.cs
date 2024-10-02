e7cbfc0ad080ea7f8068b73eb0ea6ce368fe1531
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2024-10-02 15:47:26 +0900

    弾の発射

--- Assets/Scenes/PlayerScript.cs
+++ Assets/Scenes/PlayerScript.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerScript : MonoBehaviour
 {
-    //プレイヤーの速度
-    float speed = 6.0f;
+
+    float speed = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,30 +17,34 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //プレイヤーの移動処理
-        //上に移動
+
+        // Wキー（前方移動）
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position +=  speed * transform.up * Time.deltaTime;
+            transform.position += speed * transform.up * Time.deltaTime;
         }
-        //下に移動
+
+        // Sキー（後方移動）
         if (Input.GetKey(KeyCode.S))
         {
             transform.position -= speed * transform.up * Time.deltaTime;
         }
-        //右に移動
+
+        // Dキー（右移動）
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += speed * transform.right * Time.deltaTime;
         }
-        //左に移動
+
+        // Aキー（左移動）
         if (Input.GetKey(KeyCode.A))
         {
             transform.position -= speed * transform.right * Time.deltaTime;
         }
-        
-        //弾の発射
 
 
     }
+
+    
+
 }
